06e34f7142a2ed14fc155c1bb12e500cbcfefb35
obeltramo-martin <olivier.martin@lam.fr>
2019-10-28 16:26:21 -1000

    First commit

--- _libOomao/_control/fitPowerLaw.m
+++
@@ -1,3 +0,0 @@
-function quadDiff = fitPowerLaw(telemPSD, f, params)
- quadDiff = sum(abs(telemPSD - params(2)*f.^params(1)).^2);
- 
--- _libOomao/tools.m
+++
@@ -1,2 +0,0 @@
-classdef tools < utilities & lamTools & psfrTools
-end
